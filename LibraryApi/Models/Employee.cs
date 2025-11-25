097d5a185130278d5d89274690beea6de0038142
Dylan Austin <austindylan0@gmail.com>
2025-11-25 14:24:10 +0000

    added data annotations to models

--- LibraryApi/Models/Employee.cs
+++ LibraryApi/Models/Employee.cs
@@ -1,5 +1,15 @@
 
+using System.Runtime.CompilerServices;
+using System.ComponentModel.DataAnnotations;
+
 public class EmployeeData
 {
-    
+    [Key]
+    public int Id { get; set; }          // Auto-property (getter + setter)
+
+    [Required] // Not Null
+    [StringLength(255)] // Max length
+    public string? firstName { get; set; }   // ? means can be nullable
+    public string? lastName { get; set; }
+    public string? hireDate { get; set; }
 }
