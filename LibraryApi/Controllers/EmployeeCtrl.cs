f5484d9b0bc8a0a59d44e7336713cc5b32c396f6
Dylan Austin <austindylan0@gmail.com>
2025-11-26 08:37:03 +0000

    Added basic context constructor and basic functions to EmployeeCtrl.cs (Controller)

--- LibraryApi/Controllers/EmployeeCtrl.cs
+++ LibraryApi/Controllers/EmployeeCtrl.cs
@@ -1,6 +1,7 @@
 // Co-piot informed me of what the initial set up requirements are so that the script is compiled correctly.
 // There was an error before but it needs a proper controller setup placeholder
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryApi.Controllers;  // ← Add this
 
@@ -8,9 +9,30 @@ namespace LibraryApi.Controllers;  // ← Add this
 [Route("api/[controller]")]         // ← Add this
 public class EmployeesController : ControllerBase
 {
-    [HttpGet]
-    public IActionResult Get() 
+    // Here we add a constructor to build the context
+    private readonly NewDbContext _context;
+
+    public EmployeesController(NewDbContext context)
     {
-        return Ok();  // ← Need to return something!
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<EmployeeData>>> GetEmployees()
+    {
+        return await _context.Employees.ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<EmployeeData>> GetEmployee(int id)
+    {
+        var employee = await _context.Employees.FindAsync(id);
+    
+        if (employee == null)
+        {
+            return NotFound();
+        }
+    
+        return employee;
     }
 }
